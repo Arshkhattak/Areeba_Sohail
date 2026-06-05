f94108653c1b481ffab0c590edf3174b3d344bac
Areeba Sohail <arshkhattak1@gmail.com>
2026-06-05 13:55:21 +0500

    OOD Numbers

---
+++ ODD.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+int n ;
+
+  printf("Entered the number");
+  scanf("%d" ,&n);
+for (int i = 5; i<= 50; i++){
+  if (i % 2 !=0){
+      printf("%d\n",i );
+  }
+
+}
+
+
+
+return 0;
+}
